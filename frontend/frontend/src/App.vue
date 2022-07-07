e3f47ef29fca087a195b9258d763ebeef326f110
woutswennen <wout.swennen@gmail.com>
2022-07-07 14:53:28 +0200

    Files missing to run front end

---
+++ frontend/frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app>
+        <nav-bar />
+        <v-main>
+            <v-container>
+                <router-view/>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+import navBar from "./components/navBar.vue";
+
+
+export default {
+    components: {
+        navBar,
+    }
+};
+</script>
