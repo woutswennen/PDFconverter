5fa5612519257352ec1851bdd44e5e918f77967c
woutswennen <wout.swennen@gmail.com>
2022-07-07 14:53:28 +0200

    Files missing to run front end

---
+++ frontend/frontend/src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import UploadPage from '../pages/UploadPage.vue'
+import ExperienceTable from '../components/ExperienceTable.vue'
+import EditPage from "../pages/EditPage.vue"
+import NavigationBar from "../components/navBar.vue"
+
+
+Vue.use(VueRouter);
+
+const routes = [
+ {
+   path : '/upload',
+   name : 'Upload',
+   component : UploadPage,
+ },
+ {
+   path : '/edit',
+   name : 'EditPage',
+   component : EditPage,
+ },
+ {
+    path: '/table',
+    name: 'ExperienceTable',
+    component : ExperienceTable,
+ },
+ {
+    path: '/navigation',
+    name: 'NavigationBar',
+    component : NavigationBar,
+ }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
