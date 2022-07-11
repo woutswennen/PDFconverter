2f7c41b74f7199d78e67a43852c1746dd14f50f5
woutswennen <wout.swennen@gmail.com>
2022-07-11 11:25:04 +0200

    made second project

---
+++ frontend/frontend2/src/pages/EditPage.vue
@@ -0,0 +1,126 @@
+<template>
+<div>
+  <v-form
+
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+  <b-form-row>
+    <b-column>
+        <v-text-field
+          v-model="solitan.name"
+          label="Name"
+          @blur="saveInput"
+        ></v-text-field>
+    </b-column>
+    <b-column>
+        <v-text-field
+          v-model="solitan.lastname"
+          label="Last Name"
+        ></v-text-field>
+    </b-column>
+  </b-form-row>
+  <b-form-row>
+    <v-text-field
+      v-model="solitan.birthday"
+      label="Date of birth"
+    ></v-text-field>
+
+     <v-text-field
+      v-model="solitan.nationality"
+      label="Nationality"
+    ></v-text-field>
+  </b-form-row>
+  <b-form-row>
+    <v-text-field
+      v-model="solitan.gender"
+      label="Nationality"
+    ></v-text-field>
+  </b-form-row>
+</v-form>
+
+<experience-table/>
+<certificate-table/>
+
+<button @click="renderFile">Render</button>
+
+</div>
+</template>
+
+
+
+
+
+<script>
+import { mapState } from 'vuex'
+import ExperienceTable from '../components/ExperienceTable.vue'
+import CertificateTable from '../components/CertificateTable.vue'
+import axios from 'axios'
+export default {
+  components: {
+    ExperienceTable,
+    CertificateTable,
+  },
+  computed: mapState(['solitan']),
+
+  mounted() {
+
+    this.solitan = this.$store.getters.getSolitan;
+  },
+
+  data: () => ({
+      solitan: {},
+      valid: true,
+      editedSolitan: {},
+      name: '',
+      select: null,
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4',
+      ],
+      checkbox: false,
+    }),
+
+    methods: {
+      saveInput () {
+        console.log(this.solitan.name)
+        this.$store.commit('editSolitan', this.solitan);
+      },
+      renderFile() {
+        axios({
+            method: "POST",
+            url: "http://localhost:5000/render",
+            data: this.solitan,
+            responseType: 'stream'
+        })
+        .then(async response => {
+                     var fileURL = window.URL.createObjectURL(new Blob([response.data]));
+                     var fileLink = document.createElement('a');
+
+                     fileLink.href = fileURL;
+                     fileLink.setAttribute('download', 'file.pdf');
+                     document.body.appendChild(fileLink);
+
+                     fileLink.click();
+        })
+        .catch(error => {
+          console.error("There was an error!", error);
+        });
+      }
+
+    },
+
+};
+</script>
+
+
+
+
+
+
+
+
+
---
+++ frontend/frontend2/src/pages/UploadPage.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <div>
+      <label>File
+        <input type="file" ref="fileInput" accept="image/*,.pdf" @change="handleFileUpload($event)"/>
+      </label>
+      <br/>
+      <br/>
+      <button v-on:click="submitFile()">Submit</button>
+    </div>
+  </div>
+</template>
+
+
+
+
+
+<script>
+/* eslint-disable */
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      file: null
+    }
+  },
+  methods: {
+    /**
+     * Uploads file to server.
+     * @param {Event} event The form change event with the file to be uploaded.
+     */
+    handleFileUpload(event) {
+      this.file = event.target.files[0];
+    },
+    /**
+     * Uploads the file to the server.
+     */
+    submitFile() {
+      if (this.file == null) {
+        return;
+      }
+
+      console.log("Submitting file for upload...");
+      let formData = new FormData();
+      formData.append('file', this.file);
+
+      axios.post(`http://localhost:5000/upload`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+        timeout: 5000
+      })
+        .then(response => {
+          //load the solitan object into the object store.
+          this.$store.dispatch("fetchSolitan");
+          this.$refs.fileInput.value = "";
+          console.log(response);
+        }).catch(error => {
+          console.log("File upload failed.");
+          console.error(error);
+        });
+    }
+  }
+}
+</script>
+
