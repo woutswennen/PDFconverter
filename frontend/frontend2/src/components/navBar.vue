0e713b631c574a01a17187b8c74f2571f2db669a
woutswennen <wout.swennen@gmail.com>
2022-07-11 11:25:04 +0200

    made second project

---
+++ frontend/frontend2/src/components/navBar.vue
@@ -0,0 +1,18 @@
+<template>
+ <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="/upload">Upload</b-nav-item>
+            <b-nav-item href="/edit">Edit</b-nav-item>
+          </b-navbar-nav>
+
+
+        </b-collapse>
+      </b-navbar>
+    </div>
+</template>
