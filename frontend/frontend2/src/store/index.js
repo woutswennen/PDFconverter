5dc79b59546a30e2e8ad41f7edb16262c7dfd51a
woutswennen <wout.swennen@gmail.com>
2022-07-11 12:16:02 +0200

    added package.json broke store

---
+++ frontend/frontend2/src/store/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  solitan: {}
+  },
+  getters: {
+    getSolitan(state){
+        return state.solitan
+    }
+
+  },
+  mutations: {
+    setSolitan(state, solitan){
+        state.solitan = solitan
+    },
+    setWorkExperience(state,work_experience){
+        state.solitan.work_experience = work_experience
+    }
+  },
+  actions: {
+    fetchSolitan(context){
+        return fetch("http://localhost:5000/solitan")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data)
+          context.commit("setSolitan", data.data);
+        })
+        .catch((err) => console.error(err));
+    }
+  },
+  modules: {
+  }
+})
