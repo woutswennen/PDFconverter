64f0bcc5c0f0a30a833b5921187399703fbc4f90
woutswennen <wout.swennen@gmail.com>
2022-07-13 16:31:33 +0200

    name + lastname in filename

--- frontend/src/pages/SkillPage.vue
+++ frontend/src/pages/SkillPage.vue
@@ -97,7 +97,7 @@ export default {
                      var fileLink = document.createElement('a');
 
                      fileLink.href = fileURL;
-                     fileLink.setAttribute('download', 'file.docx');
+                     fileLink.setAttribute('download', this.solitan.name + this.solitan.lastname + '.docx');
                      document.body.appendChild(fileLink);
 
                      fileLink.click();
